1dc6296ac418486ebbc9ed2b8ded18d64bfbe933
AnthonyBerisha <berishaanthony@gmail.com>
2023-05-05 14:42:28 +0200

    created calls table + model + DAL

--- src/db/init.ts
+++ src/db/init.ts
@@ -1,7 +1,9 @@
+import Call from "./models/Call";
 import Token from "./models/Token";
 
 const dbInit = async (): Promise<void> => {
   await Token.sync();
+  await Call.sync();
 };
 
 export default dbInit;
